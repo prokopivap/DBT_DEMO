7130e4b3791de295dd3c6c0633ae777f74697117
Andrii Prokopiv <propchik@ukr.net>
2026-01-09 15:37:38 +0000

    station_dimension has been added

---
+++ models/station_dimension.sql
@@ -0,0 +1,15 @@
+with bike as (
+
+select
+  distinct
+  start_statio_id as station_id,
+  start_station_name as station_name,
+  start_lat as station_lat,
+  start_lng station_lng
+from
+  {{ source('demo', 'bike') }}
+where ride_id <> 'ride_id'
+limit 10
+
+)
+select * from bike
